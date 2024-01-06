d2dc20b902b536e5e47aa78e5cf25bfdeb014f99
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-06 14:23:37 -0300

    Class 136 - Express + Webpack

---
+++ 136_express_mais_webpack/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const port = Number(3000);
+const routes = require('./routes');
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true, }));
+app.use(express.static(path.resolve(__dirname, 'public')));
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+app.use(routes);
+
+app.listen(port, () => {
+    console.log(`Servidor executando na porta ${port}`);
+    console.log(`Acessar http://localhost:${port}`);
+});
