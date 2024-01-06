42e9bd9a203f5bb3a8931e578daf78c13a517823
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-05 21:43:01 -0300

    Class 131 - Express + Nodemon

---
+++ 131_express_com_nodemon/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const port = Number(3000);
+
+app.get('/', (request, response) => {
+    response.send(`
+        <h2>
+            <a href='/contato'>Contato</a>
+        </h2>
+        <form action='/' method='POST'>
+            <label for='name'>Nome</label>
+            <input type='text' name='name' id='name' />
+            <button type='submit'>Enviar</button>
+        </form>
+    `);
+});
+
+app.post('/', (request, response) => {
+    response.send(`Recebi o fomulário`);
+});
+
+app.get('/contato', (request, response) => {
+    response.send(`
+        <h1>Página de contato</h1>
+        <h2>
+            <a href='/'>Home</a>
+        </h2>
+    `);
+});
+
+app.listen(port, () => {
+    console.log(`Servidor executando na porta ${port}`);
+    console.log(`Acessar http://localhost:${port}`);
+});
