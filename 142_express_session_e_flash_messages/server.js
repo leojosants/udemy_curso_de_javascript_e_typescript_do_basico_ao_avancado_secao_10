32de9a00b7e50aa91010e965389afa5c3069312b
Leonardo Santos <leonardojosantos@gmail.com>
2024-01-07 19:58:45 -0300

    Class 142 - Express Session and Flash Messages

---
+++ 142_express_session_e_flash_messages/server.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        app.emit('pronto');
+    })
+    .catch((error) => {
+        console.log(error);
+    });
+
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const flash = require('connect-flash');
+const port = Number(3000);
+const routes = require('./routes');
+const path = require('path');
+const { middlewareGlobal } = require('./src/middlewares/middleware');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+const sessionOptions = session({
+    secret: process.env.MYSECRET,
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true,
+    }
+});
+
+app.use(sessionOptions);
+app.use(flash());
+app.set('views', path.resolve(__dirname, 'src', 'views'));
+app.set('view engine', 'ejs');
+app.use(middlewareGlobal); // our own middleware
+app.use(routes);
+
+app.on('pronto', () => {
+    app.listen(port, () => {
+        console.log(`Acessar http://localhost:${port}`);
+        console.log(`Servidor executando na porta ${port}`);
+    });
+});
